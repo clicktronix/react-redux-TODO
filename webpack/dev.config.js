96b7bb6bc29977dd0033ed75dd8a58f3f0cdf624
clicktronix <s28.morgan@gmail.com>
2017-04-21 13:58:17 +0700

    Webpack and linters configured. Also libraries added

---
+++ webpack/dev.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const stylelint = require('stylelint');
+
+module.exports = {
+    target: 'web',
+    context: path.resolve(__dirname, '..', 'src'),
+    entry: {
+        app: './index.tsx',
+        vendor: [
+            'axios',
+            'react',
+            'redux',
+            'bem-cn',
+            'immutable',
+            'react-dom',
+            'react-redux',
+            'redux-thunk',
+            'react-router',
+        ]
+    },
+    output: {
+        publicPath: '/',
+        path: path.resolve(__dirname, '..', 'build'),
+        filename: 'js/app.bundle.js'
+    },
+    resolve: {
+        modules: ['node_modules', 'src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: [
+                    { loader: 'awesome-typescript-loader' },
+                    'tslint-loader'
+                ],
+            },
+            {
+                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: 'file-loader?name=fonts/[hash].[ext]',
+            },
+            {
+                test: /\.css$/,
+                loader:  ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    { 
+                        loader: 'style-loader!css-loader!stylus-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg)/,
+                loader: 'url-loader',
+                options: {
+                    name: 'images/[name].[ext]',
+                    limit: 10000
+                }
+            },
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'js/vendor.bundle.js',
+            minChunks: Infinity,
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'assets/index.html'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            'process.env.__HOST__': JSON.stringify('http://localhost:3000'),
+        }),
+        new webpack.NamedModulesPlugin(),
+    ],
+
+    devServer: {
+        contentBase: path.resolve('..', 'build'),
+        host: '0.0.0.0',
+        port: 8080,
+        inline: true,
+        lazy: false,
+        hot: false,
+        historyApiFallback: true,
+        stats: 'errors-only',
+    },
+};
